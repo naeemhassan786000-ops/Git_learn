d47243cb3155500d450292834bbf594c65365ee9
Hassan Ali <naeemhassan786000@gmail.com>
2026-07-17 16:44:35 +0500

    Verify

--- Git_begin.cpp
+++ Git_begin.cpp
@@ -3,4 +3,5 @@ using namespace std;
 int main(){
     cout<<"Welcome To the Git _x_ Github Master Class\n"<<endl;
     cout<<"Its My Second Version"<<endl;
+    cout<<"I m verifying a question\n";
 }
