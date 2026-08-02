5b5821bd3884b677ba59cdb75a0d396f8918bd42
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-02 20:41:58 +0500

    Commit via GitHub

--- Git_begin.cpp
+++ Git_begin.cpp
@@ -4,4 +4,5 @@ int main(){
     cout<<"Welcome To the Git _x_ Github Master Class\n"<<endl;
     cout<<"Its My Second Version"<<endl;
     cout<<"I m verifying a question\n";
+    cout<<"I am commiting from Github\n";
 }
