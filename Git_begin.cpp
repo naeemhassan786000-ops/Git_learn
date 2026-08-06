d64ce2af652ee51c56eb4b859aa4e87605521e4c
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-06 17:04:54 +0500

    feature

--- Git_begin.cpp
+++ Git_begin.cpp
@@ -1,11 +1,5 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout<<"Welcome To the Git _x_ Github Master Class\n"<<endl;
-    cout<<"Its My Second Version"<<endl;
-    cout<<"I m verifying a question\n";
-    cout<<"I am commiting from Github\n";
-    cout<<"\tEditing and Testing";
     cout<<"GitHub is amazing\n";
-
 }
