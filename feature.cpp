79478be1aabf11668379e293dbb40ef39a90829d
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-06 16:39:49 +0500

    Indent

--- feature.cpp
+++ feature.cpp
@@ -1,27 +1,28 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
-   cout<<"I will made following changes\n";
-   cout << "\t - Collision -\n"
-           "\t - Multiplayer -\n"
-           "\t - Health pipeline -\n"
-           "\t - Strikes & Attacks -\n";
-           "\t - FPS Movement -\n";
-           "\t - Combo Attacks -\n";
-           int boost=30;
-           int num;
-           cout << "Enter * count lines to make: ";
-           cin >> num;
-           for (int i = 0; i < 5; i++)
-           {
-                   if (num > 0)
-                   {
-                           for (int j = i; j < 5; j++)
-                           {
-                                   cout << "* ";
-                           }
-                           cout << "\n";
-                   }
-           }
-           return 0;
+int main()
+{
+        cout << "I will made following changes\n";
+        cout << "\t - Collision -\n"
+                "\t - Multiplayer -\n"
+                "\t - Health pipeline -\n"
+                "\t - Strikes & Attacks -\n";
+        "\t - FPS Movement -\n";
+        "\t - Combo Attacks -\n";
+        int boost = 30;
+        int num;
+        cout << "Enter * count lines to make: ";
+        cin >> num;
+        for (int i = 0; i < 5; i++)
+        {
+                if (num > 0)
+                {
+                        for (int j = i; j < 5; j++)
+                        {
+                                cout << "* ";
+                        }
+                        cout << "\n";
+                }
+        }
+        return 0;
 }
