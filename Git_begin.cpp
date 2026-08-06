89032842149a9e1c16be8e8c33b14e38bb150ac7
Hassan Ali <naeemhassan786000@gmail.com>
2026-08-06 17:01:36 +0500

    cleared

--- Git_begin.cpp
+++ Git_begin.cpp
@@ -7,17 +7,5 @@ int main(){
     cout<<"I am commiting from Github\n";
     cout<<"\tEditing and Testing";
     cout<<"GitHub is amazing\n";
-    bool move=false;
-    int i=0;
-    while(i<5){
-        if(move&&i==3){
-            cout<<"Go\n";
-        }
-        else
-        {
-            i++;
-            continue;
-        }
-    }
-    cout<<" i : "<<i<<endl;
+
 }
